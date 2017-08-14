6f1c8811b0249c4af0d5aebd35dd1a706180d540
Vytas <vdargisrobinson@gmail.com>
2017-08-13 23:57:51 -0400

    added content slider

--- node-experiment/test.js
+++ node-experiment/test.js
@@ -1,2 +1,10 @@
 var filesYo = require('fs');
+var https = require('https');
+
 filesYo.writeFile(__dirname + "/index.html", "<h1>HTML rocks</h1>");
+
+var octoUrl = "https://i.ytimg.com/vi/L-3oLTGZqNg/maxresdefault.jpg";
+var octoFile = filesYo.createWriteStream(__dirname + "/node-octo.jpg");
+var request = https.get(octoUrl, function(response) {
+	response.pipe(octoFile);
+});
